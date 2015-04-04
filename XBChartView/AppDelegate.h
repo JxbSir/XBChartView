693bee37971b3ce7892ac2f7498f87ed11d4d1ab
king824 <i@jxb.name>
2015-04-04 16:27:53 +0800

    Initial Commit

---
+++ XBChartView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBChartView
+//
+//  Created by Peter on 15/4/4.
+//  Copyright (c) 2015年 Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
